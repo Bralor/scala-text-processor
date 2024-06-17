10f4c2e7cebeb0c0b35e0ed7190c34c60f16ef57
Matouš Holinka <bralor92@email.cz>
2024-06-17 16:22:38 +0200

    main.scala: Added strip method.

--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -6,18 +6,21 @@ import java.nio.file.{Paths, Files}
   * ---
   *
   * Here comes the notes:
-  * 1. Read the TXT file
-  * 2. split the whole string
-  * 3. count the lowercase words
-  */
+  * 1. Read the TXT file,
+  * 2. split the whole string,
+  * 3. trim the undesired characters (',.:;'),
+  * 4. count the lowercase words,
+  * to-do: count all the lowercase words,
+ **/
 def main(): Unit = {
   val testFilePath: String = "src/main/resources/txt_sample.txt"
+
   val fileContent = readFileContent(filePath = testFilePath)
 
   val wordCounts = for {
     loadedText <- fileContent
-    words <- splitIntoSingleWords(stringContent = loadedText)
-    wordCounts <- mapAllWordsLength(allWords = words)
+    separatedWords <- splitIntoSingleWords(stringContent = loadedText)
+    wordCounts <- mapAllWordsLength(allWords = separatedWords)
   } yield wordCounts
 
   wordCounts match {
@@ -34,12 +37,29 @@ def readFileContent(filePath: String): Option[String] = {
   }
 }
 
+def stripSelectedStrings(stringContent: String,
+                         filtered: String = ",;:.",
+                         replacements: String = ""): Option[String] = {
+  if (stringContent.isEmpty || filtered.isEmpty) {
+    None
+  } else {
+    val filterPattern = s"^[${filtered}]+|[${filtered}]+$$"
+    Some(stringContent.replaceAll(filterPattern, replacements).trim)
+  }
+}
+
 def splitIntoSingleWords(stringContent: String,
-                         separator: String = "\\s+"): Option[Array[String]] = {
+                         separator: String = "\\s+",
+                         stripFunc: (String, String, String) => Option[String] =
+                         stripSelectedStrings): Option[Array[String]] = {
   if (stringContent.isEmpty) {
     None
   } else {
-    Some(stringContent.split(separator))
+    val separatedWords = stringContent.split(separator)
+    val cleanSeparated = separatedWords.map(
+      separatedWord => stripSelectedStrings(separatedWord).getOrElse(separatedWord)
+    )
+    Some(cleanSeparated)
   }
 }
 
